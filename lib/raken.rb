e10367518e806e6bfb812305fc311a8536ebeef0
ken <block24block@gmail.com>
2017-10-24 22:52:35 +0800

    add trace_tree

--- lib/raken.rb
+++ lib/raken.rb
@@ -1,5 +1,25 @@
 require "raken/version"
+require "rake"
+require "trace_tree"
 
-module Raken
-  # Your code goes here...
+module Rake
+  class Task
+    alias_method :_enhance, :enhance
+
+    def enhance *args, &blk
+      if block_given?
+        _enhance *args do |*params|
+          if ARGV.include? name
+            binding.trace_tree do
+              blk.call *params
+            end
+          else
+            blk.call *params
+          end
+        end
+      else
+        _enhance *args
+      end
+    end
+  end
 end
