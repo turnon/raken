58c4f144ef4c75f14a30565f01efff1c26aac829
ken <block24block@gmail.com>
2017-10-26 00:29:04 +0800

    only do trace_tree when specify `--trace-tree` and accept argument for trace_tree

--- lib/raken.rb
+++ lib/raken.rb
@@ -1,16 +1,43 @@
 require "raken/version"
 require "rake"
 require "trace_tree"
+require "json"
 
 module Rake
+  class Application
+    alias_method :_standard_rake_options, :standard_rake_options
+
+    def standard_rake_options
+      [
+        ['--trace-tree OPTS',
+         'https://github.com/turnon/trace_tree',
+         lambda { |value|
+           options.trace_tree = keyworded(value)
+         }
+        ]
+      ] + _standard_rake_options
+    end
+
+    def keyworded opt
+      JSON.parse('{' + opt.gsub(/(\w+):/, '"\1":') + '}').tap do |hash|
+        hash.keys.each do |k|
+          v = hash.delete(k)
+          hash[k.to_sym] = v
+        end
+      end
+    rescue
+      raise ArgumentError, "->#{opt}<- is not ruby keyword argument"
+    end
+  end
+
   class Task
     alias_method :_enhance, :enhance
 
     def enhance *args, &blk
       if block_given?
         _enhance *args do |*params|
-          if ARGV.include? name
-            binding.trace_tree do
+          if ARGV.include?(name) && application.options.trace_tree
+            binding.trace_tree(**application.options.trace_tree) do
               blk.call *params
             end
           else
